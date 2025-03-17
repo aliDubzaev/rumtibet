fe99077f4b86923747101b4b2c61fda70d0f5566
Ali <dubzaev98@mail.ru>
2025-03-17 05:40:02 +0300

    weq

---
+++ main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  Fancybox.bind("[data-fancybox]", {
+      Toolbar: {
+          display: ["close", "prev", "next"]
+      }
+  });
+});
+
+Fancybox.bind("[data-fancybox]", {});
+
+document.querySelector('.burger').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.menu').classList.toggle('open');
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
